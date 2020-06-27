f374fd272e2f65be5e9800fa2c05f1ee26d23098
Rishma_Mhj <rishma.maharjan1@gmail.com>
2020-06-28 00:32:32 +0545

    added query to fetch experties

--- src/main/java/com/educere/api/experties/ExpertiesRepository.java
+++ src/main/java/com/educere/api/experties/ExpertiesRepository.java
@@ -3,12 +3,14 @@ package com.educere.api.experties;
 import com.educere.api.entity.Experties;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ExpertiesRepository extends JpaRepository<Experties, Long> {
-    @Query("SELECT e FROM Experties e WHERE e.name like %?1 or e.category like %?1")
-    List<Experties> fetchAllExperties(String name, String category);
+
+    @Query("select u from Experties u where u.name like %:name% or u.category like %:category%")
+    List<Experties> fetchAllExperties(@Param("name") String name, @Param("category") String category);
 }
